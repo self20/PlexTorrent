391ad02b10d9ccc5ba0d3589cf898a6cc8fda0ec
Raul Garcia Fernandez <Raul Garcia Fernandez>
2017-06-07 10:44:03 +0200

    [Paso_Inicial]: Se ha creado un proyecto Angular4 con la aplicación basica y se ha añadido las ventanas de la posible primera version: Dashboard, Notification y New Item.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {DashboardComponent} from 'app/main-components/dashboard/dashboard.component';
+import { NewItemComponent } from './main-components/new-item/new-item.component';
+import { NotificationComponent } from './main-components/notification/notification.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'newitem', component: NewItemComponent },
+  { path: 'notification', component: NotificationComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NewItemComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
